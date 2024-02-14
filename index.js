715b32f739d64e358174dce7f6c8bdb7cc563037
Johey Hargrett <joheyhargrett@gmail.com>
2024-02-14 12:12:08 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31;
+let num2 = 2;
 
+const multiply = num1 * num2;
+
+multiply;
+
+const random = Math.floor(Math.random() + 1);
+
+random;
+
+
+const num3 = 34
+const num4 = 10
+const mod = num3 % num4;
+
+mod;
+
+
+const max = (1, 2, 3, 4, 5, 6, 7, 8, 20)
+
+Math.max(max);
 // Write your code here
